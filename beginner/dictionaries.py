8754bee9bfead55884a2a453572e286779942c48
Muhammad Hammad <itshammadofficial@gmail.com>
2025-01-21 13:07:18 +0500

    dictionary related question

--- beginner/dictionaries.py
+++ beginner/dictionaries.py
@@ -1,7 +1,7 @@
 
 # !Input :-
 ''' myinfo = {
-            server1" : {
+            "server1" : {
                     "IBM": {
                         "datacenter":"Bangalore",
                         "env": {
@@ -16,4 +16,20 @@
 # Bangalore datacenter PR address is : 192.168.1.1
 # Bangalore datacenter DR address is : 192.168.1.2
 
+myinfo = {
+    "server1": {
+        "IBM": {
+            "datacenter": "Bangalore",
+            "env": {
+                "PR": "192.168.1.1",
+                "DR": "192.168.1.2"
+            }
+        }
+    }
+}
 
+bangalore_PR = myinfo["server1"]["IBM"]["env"]['PR']
+bangalore_DR = myinfo["server1"]["IBM"]["env"]['DR']
+
+print(f"Bangalore datacenter PR address is : {bangalore_PR}")
+print(f"Bangalore datacenter DR address is : {bangalore_DR}")
